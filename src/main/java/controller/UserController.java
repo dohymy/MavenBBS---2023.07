3d8d2f1e230308da40eb9b95b10e4a706b4f2d10
Do Hyun Myung <dohymywork@gmail.com>
2023-07-17 17:36:55 +0900

    resister

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -12,12 +12,35 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class UserController
  */
-@WebServlet("/*")
+@WebServlet("/user/*")
 public class UserController extends HttpServlet {
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/hello.jsp");
-		rd.forward(request, response);
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] uri = request.getRequestURI().split("/");
+		String action = uri[uri.length - 1];
+		
+		RequestDispatcher rd = null;
+		switch (action) {
+		case "list":
+			rd = request.getRequestDispatcher("/WEB-INF/view/user/list.jsp");
+			rd.forward(request, response);
+			break;
+		case "login":
+			if (request.getMethod().equals("GET")) {
+				rd = request.getRequestDispatcher("/WEB-INF/view/user/login.jsp");
+				rd.forward(request, response);
+			} else {
+				
+			}
+			break;
+		case "resister":
+			if (request.getMethod().equals("GET")) {
+				rd = request.getRequestDispatcher("/WEB-INF/view/user/resister.jsp");
+				rd.forward(request, response);
+			} else {
+				
+			}
+			break;
+		}
 	}
-
 }
